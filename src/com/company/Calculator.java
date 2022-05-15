a4488b02ded9c3769f0850f9043afc6fe9098e03
berdibekkurbanbaev <kurbanbaevberdibek@gmail.com>
2022-05-15 21:45:29 +0300

    Initial commit

---
+++ src/com/company/Calculator.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Calculator {
+    private Calculator() {
+    }
+
+    public static int calculate(int number1, int number2, char operation) {
+        int result = 0;
+        switch (operation) {
+            case '+':
+                result = number1 + number2;
+                break;
+            case '-':
+                result = number1 - number2;
+                break;
+            case '*':
+                result = number1 * number2;
+                break;
+            case '/':
+                result = number1 / number2;
+                break;
+            default:
+                throw new IllegalArgumentException("Не верный знак операции");
+        }
+        return result;
+    }
+
+}
